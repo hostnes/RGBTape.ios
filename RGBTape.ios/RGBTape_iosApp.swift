cf0991e9d92bdba0f60aacb7eecc55c4c57d5ab6
hostnes <lapanik2708@gmail.com>
2024-01-10 18:59:03 +0300

    Initial Commit

---
+++ RGBTape.ios/RGBTape_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBTape_iosApp.swift
+//  RGBTape.ios
+//
+//  Created by hostnes on 10/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct RGBTape_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
